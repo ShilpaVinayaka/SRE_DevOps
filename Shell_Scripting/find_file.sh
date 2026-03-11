965693c26a26899a5b01981fa4cbe4aef8934847
ShilpaVinayaka <shilpaparanjape2007@gmail.com>
2026-03-11 16:24:26 +0530

    Detail

--- Shell_Scripting/find_file.sh
+++ Shell_Scripting/find_file.sh
@@ -1,6 +1,14 @@
 # A server’s disk is getting full frequently.
 # Find files larger than 100MB
 
-#!/bin/bash
+#!/bin/sh
 
 find /var/log -type f -size +100M -mtime +7 -exec ls -lh {} \;
+
+# 🧠 Step-by-step logic
+
+# find → to search files
+# -type f → only files
+# -size +100M → larger than 100MB
+# -mtime +7 → older than 7 days
+# -exec ls -lh → human-readable size
